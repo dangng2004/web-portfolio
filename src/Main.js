d6611fc21f85f7cca1aafed812eebc3bffa669ab
Dang Nguyen <nmd.ptnk@gmail.com>
2020-12-22 19:12:13 -0600

    added resume page, deleted EE page

--- src/Main.js
+++ src/Main.js
@@ -10,7 +10,6 @@ import Software from './Software/Software';
 import Art from './Art/Art';
 import Cooking from './Cooking/Cooking';
 import Resume from './Resume/Resume';
-import ExtendedEssay from './EE/ExtendedEssay';
 
 import './Main.css'
 
@@ -18,27 +17,27 @@ class Main extends React.Component {
   render() {
     return (
       <HashRouter>
-        <div>
-          <ul className="navbar">
-            <li><NavLink exact to="/">Home</NavLink></li>
-            <li><NavLink to="/software">Software</NavLink></li>
-            <li><NavLink to="/art">Art</NavLink></li>
-            <li><NavLink to="/cooking">Cooking</NavLink></li>
-            <li><NavLink to="/resume">Resume</NavLink></li>
-            <li><NavLink to="/ee">IB Extended Essay</NavLink></li>
-          </ul>
+        <div className="screen">
           <div className="content">
-            <Route exact path="/" component={Home}/>
-            <Route path="/software" component={Software}/>
-            <Route path="/art" component={Art}/>
-            <Route path="/cooking" component={Cooking}/>
-            <Route path="/resume" component={Resume}/>
-            <Route path="/ee" component={ExtendedEssay}/>
+            <ul className="navbar">
+              <li><NavLink exact to="/">Home</NavLink></li>
+              <li><NavLink to="/software">Software</NavLink></li>
+              <li><NavLink to="/art">Art</NavLink></li>
+              <li><NavLink to="/cooking">Cooking</NavLink></li>
+              <li><NavLink to="/resume">Resume</NavLink></li>
+            </ul>
+            <div className="pages">
+              <Route exact path="/" component={Home}/>
+              <Route path="/software" component={Software}/>
+              <Route path="/art" component={Art}/>
+              <Route path="/cooking" component={Cooking}/>
+              <Route path="/resume" component={Resume}/>
+            </div>
           </div>
-        </div>
-        <div className="footer">
-            <p>Phone: +1 (773) 936-0857</p>
-            <p>Email: dangnguyen@uchicago.edu</p>
+          <div className="footer">
+              <p>Phone: +1 (773) 936-0857</p>
+              <p>Email: dangnguyen@uchicago.edu</p>
+          </div>
         </div>
       </HashRouter>
     );
--- src/Resume/Resume.js
+++ src/Resume/Resume.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import resume from './DangNguyen_Resume_CS.pdf';
+import './Resume.css'
 
 class Resume extends React.Component {
   render() {
     return (
-      <div>
-        <p>Welcome to the resume page</p>
+      <div className="resume-page">
+        <embed className="resume" src={resume}></embed>
       </div>
     );
   }
